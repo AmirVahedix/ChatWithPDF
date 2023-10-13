dbccba90a695e2d5fc2b58011a85bc8402c1c8a5
= <=>
2023-10-13 23:26:12 +0330

    Created MaxWidthWrapper component and cn util for merging classnames

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen font-sans antialiased grainy">
+        {children}
+      </body>
     </html>
   );
 }
