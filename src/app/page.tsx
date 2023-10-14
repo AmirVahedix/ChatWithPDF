5802192af863de25f6c6085c5891e1f7b74eb242
= <=>
2023-10-14 22:10:13 +0330

    Added hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRight } from "lucide-react";
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -45,6 +46,22 @@ export default function Home() {
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
+          </div>
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src={"/dashboard-preview.jpg"}
+                    width={1364}
+                    height={866}
+                    alt="product preview"
+                    quality={100}
+                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
