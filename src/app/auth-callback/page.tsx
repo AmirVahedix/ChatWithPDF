83016931579e9fd2db0f4e1fd2bda8ca52749f4b
= <=>
2023-10-18 19:51:50 +0330

    Implemented authCallback tRPC procedure

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,11 +1,19 @@
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
+import { trpc } from "../_trpc/client";
 
 const AuthCallbackPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const origin = searchParams.get("origin");
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (!success) return;
+      router.push(origin ? `/${origin}` : "/dashboard");
+    },
+  });
 
   return <div>AuthCallbackPage</div>;
 };
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,17 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  test: publicProcedure.query(() => {
-    return 2;
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    return { success: true };
   }),
 });
 
