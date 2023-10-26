658f21a930647ca1d619afe1cb233f0660a861bd
= <=>
2023-10-26 13:39:11 +0330

    Store uploaded file data in database, accept only PDF files in upload-thing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,11 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { db } from "@/db";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = getUser();
@@ -14,7 +15,15 @@ export const ourFileRouter = {
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-        
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://utfs.io/f/${file.key}`,
+          uploadStatus: "PROCESSING",
+        },
+      });
     }),
 } satisfies FileRouter;
 
