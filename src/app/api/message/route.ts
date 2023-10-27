8035d66f7dc6f420b20a5201c8c779463b27d5c1
= <=>
2023-10-27 18:58:00 +0330

    Created message endpoint

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,24 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const { id: userId } = getUser();
+
+  if (!userId) return new Response("Unauthorized", { status: 401 });
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) return new Response("Not found", { status: 404 });
+};
